96851df6ff86fc37b413980e0f7d683acb777115
Daniele Mazzotta <30733754+DanieleMazzotta@users.noreply.github.com>
2026-02-09 13:24:02 +0000

    init commit

--- exercises/ch01_getting_started/ex01_hello_world/solutions/reference.rs
+++
@@ -1,8 +0,0 @@
-// Reference solution for Hello, World!
-//
-// This is the canonical solution that demonstrates the basic structure
-// of a Rust program and proper use of the println! macro.
-
-fn main() {
-    println!("Hello, world!");
-}
--- exercises/ch05_using_structs/ex07_rectangle_program/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// TODO: Define a Rectangle struct with width and height fields (u32)
-
-// TODO: Implement Debug trait for Rectangle using #[derive(Debug)]
-
-// TODO: Implement an area method for Rectangle in an impl block
-
-// TODO: Implement a can_hold method that takes another Rectangle by reference
